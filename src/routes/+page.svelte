5a8003b6647e983c723a42755e019c0282b2406b
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-07-24 22:55:52 +0700

    add Skeleton UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<br />
+
+<a href="/login" class="btn variant-filled">Login</a>
+<br />
+<a href="/register" class="btn variant-filled mt-2">Register</a>
