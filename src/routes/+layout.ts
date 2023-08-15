fb9abc5c193771f1123841f7fc4cc409be939a9b
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-08-15 22:44:17 +0700

    yeah

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import { redirect, type LoadEvent } from '@sveltejs/kit';
 import { authStore } from '$stores/authStore';
 import { get } from 'svelte/store';
 
+// TODO: this doesn't work
 export function load({ url }: LoadEvent) {
 	console.log('url', url);
 	
