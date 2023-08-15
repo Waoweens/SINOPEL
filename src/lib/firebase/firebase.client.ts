fd3ea283f75be393650167c52435f5518ac87a5c
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-08-15 22:44:38 +0700

    change env vars names

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -10,13 +10,13 @@ import { onMount } from 'svelte';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_APIKEY,
-	authDomain: import.meta.env.VITE_AUTHDOMAIN,
-	projectId: import.meta.env.VITE_PROJECTID,
-	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-	appId: import.meta.env.VITE_APPID,
-	measurementId: import.meta.env.VITE_MEASUREMENTID
+	apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_FIREBASE_APPID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
