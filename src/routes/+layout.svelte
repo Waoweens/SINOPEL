8e4ee8c230d348f14434771a0a0708a44d4d94b5
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-08-20 16:43:32 +0700

    lets use sass instead

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import '../app.postcss';
+	import '../app.scss';
 	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// This contains the bulk of Skeletons required styles:
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
-	import '../app.postcss';
+	import '../app.scss';
 
 	import { AppShell, Modal, Toast, drawerStore, storePopup } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
