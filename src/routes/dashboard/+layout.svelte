883e25a10948e075b132cee440107cbb6cc1070f
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-08-20 16:41:02 +0700

    move auth check to layout

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { authStore } from "$stores/authStore";
+	import { ProgressRadial } from "@skeletonlabs/skeleton";
+
+</script>
+
+{#if $authStore.currentUser}
+	<slot />
+{:else}
+	<div class="flex h-screen">
+		<div class="m-auto">
+			<ProgressRadial stroke={100} />
+		</div>
+	</div>
+{/if}
--- src/routes/dashboard/calendar/+page.svelte
+++ src/routes/dashboard/calendar/+page.svelte
@@ -14,15 +14,7 @@
 	<title>Kalender - {appname}</title>
 </svelte:head>
 
-{#if $authStore.currentUser}
-	<h1 class="text-2xl font-bold">Selamat datang, {'{'}username{'}'}</h1>
-	<div class="max-w-3xl mt-2 max-h-none rounded-lg p-4 bg-surface-50-900-token">
-		<Calendar />
-	</div>
-{:else}
-	<div class="flex h-screen">
-		<div class="m-auto">
-			<ProgressRadial stroke={100} />
-		</div>
-	</div>
-{/if}
+<h1 class="text-2xl font-bold">Selamat datang, {'{'}email{'}'}</h1>
+<div class="max-w-3xl mt-2 max-h-none rounded-lg p-4 bg-surface-50-900-token">
+	<Calendar />
+</div>
