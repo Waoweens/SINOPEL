bd05fe810a21367f3f8238d149ec254a36e6ae15
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-08-29 22:30:30 +0700

    admin navbar

--- src/routes/dashboard/admin/+layout.svelte
+++ src/routes/dashboard/admin/+layout.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailAnchor, TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
 	import { Doc, SignedIn } from 'sveltefire';
 </script>
 
 <SignedIn let:user>
 	<Doc ref={'/users/' + user.uid} let:data>
 		{#if data.role == 'admin'}
-			<AppRail>
-				<AppRailAnchor href="/dashboard/admin" selected={$page.url.pathname === '/dashboard/admin'}>
-					<span>Admin</span>
-				</AppRailAnchor>
-			</AppRail>
+			<TabGroup>
+				<TabAnchor href="/dashboard/admin" selected={$page.url.pathname === '/dashboard/admin'}>
+					<span>Home</span>
+				</TabAnchor>
+				<TabAnchor
+					href="/dashboard/admin/users"
+					selected={$page.url.pathname === '/dashboard/admin/users'}
+				>
+					<span>Akun</span>
+				</TabAnchor>
+				<TabAnchor
+					href="/dashboard/admin/employees"
+					selected={$page.url.pathname === '/dashboard/admin/employees'}
+				>
+					<span>Pejabat</span>
+				</TabAnchor>
+			</TabGroup>
 			<slot />
 		{:else}
 			<h1>403</h1>
--- src/routes/dashboard/admin/+page.svelte
+++ src/routes/dashboard/admin/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-
+	import { page } from "$app/stores";
+	import { TabAnchor, TabGroup } from "@skeletonlabs/skeleton";
 </script>
 
 <h1>admin</h1>
