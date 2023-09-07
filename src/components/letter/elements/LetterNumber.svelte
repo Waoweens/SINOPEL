71389c056c1d3ba7b05f859800e7ac3176424cc2
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-09-08 00:21:40 +0700

    new LetterNumber input component

---
+++ src/components/letter/elements/LetterNumber.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div class="grid grid-cols-[1fr_auto] gap-y-1 lg:flex lg:flex-row">
+	<input class="input rounded-r-none" placeholder="[0]" />
+	<span class="x-input-group-seperator">/</span>
+
+	<input class="input lg:rounded-l-none rounded-r-none" placeholder="[1]" />
+	<span class="x-input-group-seperator">/</span>
+
+	<input class="input lg:rounded-l-none rounded-r-none" placeholder="[2]" />
+	<span class="x-input-group-seperator">-</span>
+
+	<input class="input lg:rounded-l-none rounded-r-none" placeholder="[3]" />
+	<span class="x-input-group-seperator">/</span>
+
+	<input class="input lg:rounded-l-none rounded-r-none" placeholder="[4]" />
+	<span class="x-input-group-seperator">/</span>
+
+	<input class="input lg:rounded-l-none col-span-2" placeholder="[5]" />
+</div>
