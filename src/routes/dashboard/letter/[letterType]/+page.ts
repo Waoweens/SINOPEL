2c55bc4391cfe21075895c616b8e24d6970a61bd
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-09-11 00:45:48 +0700

    new table page

--- src/routes/dashboard/letter/[letterType]/+page.ts
+++ src/routes/dashboard/letter/[letterType]/+page.ts
@@ -1,6 +1,13 @@
-import type { PageLoad } from "./$types";
+import { isLetterType } from '$lib/letter';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params }) => {
+
+	if (!isLetterType(params.letterType)) {
+		throw error(404, 'Not found');
+	}
+
 	return {
 		letter: {
 			type: params.letterType
