b7a5947b6abdd25eb98b025180d725f7a075da36
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-09-14 23:03:11 +0700

    get letter data to layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,8 @@
 // import { authStore } from '$stores/authStore';
 // import { get } from 'svelte/store';
 
+import type { PageLoad } from "./dashboard/letter/[letterType]/edit/[letterId]/$types";
+
 // // TODO: this doesn't work
 // export function load({ url }: LoadEvent) {
 // 	console.log('url', url);
@@ -9,3 +11,12 @@
 // 	const authValue = get(authStore);
 // 	console.log('authValue', authValue);
 // }
+
+export const load: PageLoad = ({ params }) => {
+	return {
+		letter: {
+			type: params.letterType,
+			id: params.letterId
+		}
+	};
+};
