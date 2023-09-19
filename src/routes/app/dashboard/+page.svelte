2254669f5f124acc7978059d04fa5746f3ad6eb2
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-09-19 13:07:03 +0700

    add text to dashboard

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { appname } from "$stores/static";
+	import { appname, appnameLong } from '$stores/static';
 </script>
 
 <svelte:head>
 	<title>Dasbor - {appname}</title>
 </svelte:head>
 
-<h1>Dasbor</h1>
+<div class="w-full h-full pb-32 flex flex-col items-center justify-center text-center">
+	<h1 class="text-6xl lg:text-8xl font-extrabold">{appname}</h1>
+	<p class="text-2xl md:text-4xl font-semibold">{appnameLong}</p>
+</div>
