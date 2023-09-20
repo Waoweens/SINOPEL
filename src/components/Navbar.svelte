3fb6d177afb3c86564e431ca5129e45497d7ff5f
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-09-20 16:04:38 +0700

    add info panel

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,6 +3,12 @@
 	import { appname } from '$stores/static';
 	import IconAccount from '~icons/ic/baseline-account-circle';
 	import IconApps from '~icons/ic/baseline-apps';
+	import IconInfo from '~icons/ic/baseline-info';
+	import IconCode from '~icons/ic/baseline-code';
+	import IconBugReport from '~icons/ic/baseline-bug-report';
+	import IconCopyright from '~icons/ic/baseline-copyright';
+	import IconPerson from '~icons/ic/baseline-person';
+	import IconSourceBranch from '~icons/mdi/source-branch';
 	import { navrailState } from '$stores/states';
 	import { Doc, SignedIn, SignedOut } from 'sveltefire';
 	import LightSwitchX from './elements/LightSwitchX.svelte';
@@ -11,6 +17,12 @@
 	const popupUser: PopupSettings = {
 		event: 'click',
 		target: 'popupUser',
+		placement: 'bottom-end'
+	};
+
+	const popupInfo: PopupSettings = {
+		event: 'click',
+		target: 'popupInfo',
 		placement: 'bottom-end'
 	};
 </script>
@@ -29,6 +41,66 @@
 	</svelte:fragment>
 	{$appname}
 	<svelte:fragment slot="trail">
+		<button type="button" class="btn p-0 -my-2" use:popup={popupInfo}>
+			<IconInfo class="text-3xl" />
+		</button>
+
+		<div class="card p-4 w-60 shadow-xl" data-popup="popupInfo">
+			<p class="flex items-center gap-2">
+				<span class="text-2xl font-bold">SINOPEL</span><a
+					target="_blank"
+					href="https://github.com/Waoweens/SINOPEL/releases/tag/v1.0-rc1"
+					class="chip variant-filled-secondary">v1.0-rc1</a
+				>
+			</p>
+			<hr class="my-3" />
+			<nav class="list-nav m-0 p-0">
+				<ul class="m-0 p-0">
+					<li>
+						<a target="_blank" href="https://github.com/Waoweens/SINOPEL">
+							<span><IconCode /></span>
+							<span>Repository</span>
+						</a>
+						<a target="_blank" href="https://github.com/Waoweens/SINOPEL">
+							<span><IconBugReport /></span>
+							<span>Lapor masalah</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+
+			<hr class="my-3" />
+
+			<nav class="list-nav m-0 p-0">
+				<ul class="m-0 p-0">
+					<li>
+						<button>
+							<span><IconSourceBranch /></span>
+							<span>Change Branch</span>
+						</button>
+					</li>
+				</ul>
+			</nav>
+
+			<hr class="my-3" />
+
+			<nav class="list-nav m-0 p-0">
+				<ul class="m-0 p-0">
+					<li>
+						<a target="_blank" href="https://github.com/Waoweens">
+							<span><IconPerson /></span>
+							<span>View Developer</span>
+						</a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.gnu.org/licenses/gpl-3.0.en.html">
+							<span><IconCopyright /></span>
+							<span>GNU GPL v3.0</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 		<LightSwitchX />
 
 		<button type="button" class="btn p-0 -my-2" use:popup={popupUser}>
@@ -49,7 +121,7 @@
 				<hr class="my-3" />
 				<nav class="list-nav m-0 p-0">
 					<ul class="m-0 p-0">
-						<li class="m-0 p-0">
+						<li>
 							<a href="/app/dashboard">Dasbor</a>
 						</li>
 						<li>
